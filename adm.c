126ca82f9282e92f1f6f02e03b958eea5dc6dbae
pedroleite21 <pedro.leite.001@acad.pucrs.br>
2019-03-30 18:06:38 -0300

    Caixa eletronico pronto

--- adm.c
+++ adm.c
@@ -6,59 +6,101 @@
 
 #define MAX_CONTAS 10
 
-typedef struct {
+typedef struct
+{
 	char idConta[10];
 	double saldo;
+	double senha;
 } CONTA;
+
+char *contaAtual;
+
+double valorsaque;
 
 double *verifica_conta(char **idConta)
 {
 	static CONTA contas[MAX_CONTAS] = {
-		{"1-1", 1520.63},
-		{"1-2", 63.2}
-	};	
+		{"1-1", 1520.63, 1},
+		{"1-2", 63.2, 1}};
 	static double ok = 1.0;
 	static double erro = -1.0;
+	static double valor = 1000.20;
 	int i;
+	float l;
+	char pd[20];
 
-	for(i=0; i<MAX_CONTAS; i++) {
-		if(strcmp(contas[i].idConta, *idConta) == 0) {
-			return &(ok);
+	sscanf((*idConta),"%s %f", pd, &l);
+
+	if (strcmp(pd, "saldo") == 0)
+	{
+		for (i = 0; i < MAX_CONTAS; i++)
+		{
+			if (strcmp(contas[i].idConta, contaAtual) == 0)
+			{
+				return &(contas[i].saldo);
+			}
 		}
+		return &erro;
+	} else if(strcmp(pd, "saque") == 0) {
+		
+		sscanf(*idConta, "saque %lf", &valorsaque);
 
+		return &(valorsaque);
+	} else if(strcmp(pd, "confirmasaque") == 0) {
+		
+		for (i = 0; i < MAX_CONTAS; i++)
+		{
+			if (strcmp(contas[i].idConta, contaAtual) == 0)
+			{
+				if (((contas[i].saldo - valorsaque) > 0.0) && (l == contas[i].senha))
+				{
+					contas[i].saldo = contas[i].saldo - valorsaque;
+					return &(contas[i].saldo);
+				}
+			}
+		}
+		return &(erro);
+	} else if(strcmp(pd, "deposito") == 0) {
+		
+		sscanf(*idConta, "deposito %lf", &valorsaque);
+
+		return &(valorsaque);
+	} else if(strcmp(pd, "confirmadeposito") == 0) {
+		
+		for (i = 0; i < MAX_CONTAS; i++)
+		{
+			if (strcmp(contas[i].idConta, contaAtual) == 0)
+			{
+				if (((contas[i].saldo + valorsaque) > 0.0) && (l == contas[i].senha))
+				{
+					contas[i].saldo = contas[i].saldo + valorsaque;
+					return &(contas[i].saldo);
+				}
+			}
+		}
+		return &(erro);
+	} else {
+		contaAtual = *idConta;
+		for (i = 0; i < MAX_CONTAS; i++)
+		{
+			if (strcmp(contas[i].idConta, *idConta) == 0)
+			{
+				return &(ok);
+			}
+		}
+		return &erro;
 	}
-	return &erro;
 }
 
-double *obtem_saldo(char **idConta)
+int main()
 {
-	static CONTA contas[MAX_CONTAS] = {
-		{"1-1", 1520.63},
-		{"1-2", 63.2}
-	};	
-	static double ok = 1.0;
-	static double erro = -1.0;
-	int i;
-
-	for(i=0; i<MAX_CONTAS; i++) {
-		if(strcmp(contas[i].idConta, *idConta) == 0) {
-			return &(contas[i].saldo);
-		}
-
-	}
-	return &erro;
-}
-
-int main() {
-	if (registerrpc(VCPROG,VCVERS,VC,verifica_conta, (xdrproc_t)xdr_wrapstring,(xdrproc_t)xdr_double) == -1)  {
-		fprintf(stderr,"Erro em registerrpc()!\n");
+	if (registerrpc(VCPROG, VCVERS, VC, verifica_conta, (xdrproc_t)xdr_wrapstring, (xdrproc_t)xdr_double) == -1)
+	{
+		fprintf(stderr, "Erro em registerrpc()!\n");
 		exit(1);
 	}
 
 	svc_run();
-	fprintf(stderr,"Erro em svc_run()!\n");
+	fprintf(stderr, "Erro em svc_run()!\n");
 	return 1;
-
 }
-
-
--- caixa.c
+++ caixa.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <rpc/rpc.h>
 #include "const.h"
+
+void printOpcoes()
+{
+	printf("O que deseja fazer?\n\n");
+	printf("1 - Verificar Saldo   3 - Realizar Depósito\n");
+	printf("2 - Realizar Saque    4 - Sair    \n");
+}
 
 int main(int argc, char *argv[]) 
 {
 	char *pd;
 	int stat;
 	double saldo;
+	float saque;
+	char valor[30];
 
-	char numconta[10];
+	char *numconta;
+	char *server;
 	int opcao;
-	
+	float senha;
+
 	if (argc != 3) {
 		fprintf(stderr, "Uso: \n%s <nome_do_servidor> <conta>\n\n", argv[0]);
 		return 1;
 	}
 	pd = argv[2];
-	printf("%s", pd);
+	server = argv[1];
+	numconta =  argv[2];
 	stat = callrpc(argv[1], VCPROG, VCVERS, VC,
 		(xdrproc_t)xdr_wrapstring,(char *)&pd, 
 		(xdrproc_t)xdr_double, (char *)&saldo);
@@ -25,17 +38,15 @@ int main(int argc, char *argv[])
 		printf("\n");
 		return 1;	
 	}
-	strcpy(numconta, argv[2]);
 	if(saldo < 0.0)
 		printf("Resultado: conta não encontrada!\n");
 	else {
 		printf("/*****************************************/\n");
 		printf("/           BEM VINDO AO BANCO POC        /\n");
 		printf("/*****************************************/\n");
-		printf("Conta: %s\n\n", argv[2]);
-		printf("O que deseja fazer?\n\n");
-		printf("1 - Verificar Saldo   3 - Realizar Depósito\n");
-		printf("2 - Realizar Saque    4 - Sair    \n");	
+		printf("Conta: %s\n\n", numconta);
+		printOpcoes();
+
 
 		while(1) {
 			fflush(stdin);
@@ -44,19 +55,86 @@ int main(int argc, char *argv[])
 			switch(opcao) 
 			{
 				case 1:
-					printf("Aguarde...\n");	
+					printf("Aguarde...\n");
+					pd = "saldo 10.23";
+					stat = callrpc(server, VCPROG, VCVERS, VC,
+								   (xdrproc_t)xdr_wrapstring, (char *)&pd,
+								   (xdrproc_t)xdr_double, (char *)&saldo);
+
+					if(saldo >= 0.0) {
+						printf("Seu saldo: R$ %.2f\n\n", saldo);
+
+						printf("Deseja realizar outra operaçao?\nSe sim, digite 5. Se deseja sair, digite 4.\n");
+					} else {
+						printf("Oops... algum erro se deu por aqui");
+					}
 
 					break;
 				case 2:
-					printf("Realizar Saque");
+					printf("Digite o valor desejado: ");
+					scanf("%f", &saque);
+
+					sprintf(valor, "saque %.2f", saque);
+
+					pd = valor;
+					stat = callrpc(server, VCPROG, VCVERS, VC,
+								   (xdrproc_t)xdr_wrapstring, (char *)&pd,
+								   (xdrproc_t)xdr_double, (char *)&saldo);
+
+					printf("Confirmar saque de: R$ %.2f?\nDigite a senha: ", saldo);
+					scanf("%f", &senha);
+
+					sprintf(valor, "confirmasaque %.1f", senha);
+					pd = valor;
+					stat = callrpc(server, VCPROG, VCVERS, VC,
+								   (xdrproc_t)xdr_wrapstring, (char *)&pd,
+								   (xdrproc_t)xdr_double, (char *)&saldo);
+
+					if(saldo > 0.0) {
+						printf("Ok! O seu dinheiro já tá saindo :)\n");
+						printf("Deseja realizar outra operaçao?\nSe sim, digite 5. Se deseja sair, digite 4.\n");
+					} else {
+						printf("Oops... algum erro se deu por aqui\n");
+						exit(1);
+					}
 					break;
 				case 3:
-					printf("Realizar Depósito");
+					printf("Digite o valor desejado: ");
+					scanf("%f", &saque);
+
+					sprintf(valor, "deposito %.2f", saque);
+					pd = valor;
+					stat = callrpc(server, VCPROG, VCVERS, VC,
+								   (xdrproc_t)xdr_wrapstring, (char *)&pd,
+								   (xdrproc_t)xdr_double, (char *)&saldo);
+
+					printf("Confirmar depósito de: R$ %.2f?\nDigite a senha: ", saldo);
+					scanf("%f", &senha);
+
+					sprintf(valor, "confirmadeposito %.1f", senha);
+					pd = valor;
+					stat = callrpc(server, VCPROG, VCVERS, VC,
+								   (xdrproc_t)xdr_wrapstring, (char *)&pd,
+								   (xdrproc_t)xdr_double, (char *)&saldo);
+
+					if(saldo > 0.0) {
+						printf("Ok! O seu dinheiro já tá na conta :)\n");
+						printf("Deseja realizar outra operaçao?\nSe sim, digite 5. Se deseja sair, digite 4.\n");
+					} else {
+						printf("Oops... algum erro se deu por aqui\n");
+						exit(1);
+					}
 					break;
 				case 4:
 					exit(1);
+				case 5:
+					printOpcoes();
+					break;
 				default:
 					printf("Opção inválida!!!\n");
+
+					printOpcoes();
+					break;
 			}
 		}
 	}
