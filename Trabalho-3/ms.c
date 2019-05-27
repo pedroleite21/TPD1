ba6f3f42e1e74f156e5a75d1732c8b20a25058de
Pedro Leite <pedro.passos@pucrs.br>
2019-05-27 10:54:01 -0300

    Almost

--- Trabalho-3/ms.c
+++ Trabalho-3/ms.c
@@ -6,60 +6,105 @@
 #define M 1000
 #define N 100000
 
+#define TRABALHO 1
+#define KILL 2
+
+int cmpfunc(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
+}
+
 int main(int argc, char **argv)
 {
+    double t_inicial, t_final; // marcador de tempo
     int i, j;
-    int l = 1000, c = N;
-    int my_rank; // Identificador deste processo
-    int proc_n;  // Numero de processos disparados pelo usuario na linha de comando (np)
-    //int message; // Buffer para as mensagens
-    //int saco[10][N];      // saco de trabalho
+    int l = M, c = N;
+    int my_rank;       // Identificador deste processo
+    int proc_n;        // Numero de processos disparados pelo usuario na linha de comando (np)
     MPI_Status status; // estrutura que guarda o estado de retorno
 
-    // inicializo o saco de trabalho
-    int(*saco)[N] = malloc(M * sizeof *saco);
+    int(*saco)[c] = malloc(l * sizeof *saco);
+    int(*message) = malloc(c * sizeof(int));
+    int *proc_control;
 
     for (i = 0; i < l; i++)
         for (j = 0; j < c; j++)
-            saco[i][j] = i*j;
+            saco[i][j] = c - j;
 
     MPI_Init(&argc, &argv); // funcao que inicializa o MPI, todo o codigo paralelo estah abaixo
 
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); // pega pega o numero do processo atual (rank)
     MPI_Comm_size(MPI_COMM_WORLD, &proc_n);  // pega informacao do numero de processos (quantidade total)
 
-    // if (my_rank == 0)
-    // {
-    //     // MESTRE
-    //     printf("\nMestre[%d]: ", my_rank);
-    //     for (i = 0; i < 15; printf("%d ", saco[i++]))
-    //         ;
+    proc_control = calloc(proc_n - 1, sizeof *proc_control);
 
-    //     // mando o trabalho para os escravos fazerem
+    int tarefa, position;
 
-    //     for (i = 1; i < proc_n; i++)
-    //     {
-    //         message = saco[i - 1];
-    //         MPI_Send(&message, 1, MPI_INT, i, 1, MPI_COMM_WORLD); // envio trabalho saco[i-1] para escravo com id = i
-    //     }
+    if (my_rank == 0)
+    {
+        // MESTRE
+        t_inicial = MPI_Wtime();
 
-    //     // recebo o resultado
+        //ENVIA PRIMEIRAS TAREFAS
+        for (i = 1; i < proc_n; i++)
+        {
+            proc_control[i - 1] = i;
+            MPI_Send(saco[i - 1], c, MPI_INT, i, TRABALHO, MPI_COMM_WORLD);
+            printf("[Mestre] : Enviando tarefa para [%d]\n", i);
+        }
 
-    //     for (i = 1; i < proc_n; i++)
-    //     {
-    //         // recebo mensagens de qualquer emissor e com qualquer etiqueta (TAG)
+        tarefa = (proc_n - 1) - 1;
+        while (++tarefa < 1)
+        {
+            MPI_Probe(MPI_ANY_SOURCE, TRABALHO, MPI_COMM_WORLD, &status);
+            position = proc_control[status.MPI_SOURCE - 1];
+            MPI_Recv(saco[position], c, MPI_INT, MPI_ANY_SOURCE, TRABALHO, MPI_COMM_WORLD, &status);
 
-    //     // executo opera��o
+            printf("[Mestre] : Recebi tarefa completa de [%d]\n", position);
 
-    //     printf("\nEscravo[%d]: recebi %d", my_rank, message);
-    //     message = my_rank; // devolvo meu pid para o mestre
+            proc_control[status.MPI_SOURCE - 1] = tarefa;
+            MPI_Send(saco[tarefa], c, MPI_INT, status.MPI_SOURCE, TRABALHO, MPI_COMM_WORLD);
 
-    //     // retorno resultado para o mestre
+            printf("[Mestre] : Enviando nova tarefa para [%d]\n", status.MPI_SOURCE);
+        }
 
-    //     MPI_Send(&message, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
-    // }
+        for (i = 0; i < proc_n - 1; i++)
+        {
+            if (proc_control[i] != 0)
+            {
+                position = proc_control[i];
+                MPI_Recv(saco[position], c, MPI_INT, i + 1, TRABALHO, MPI_COMM_WORLD, &status);
+                proc_control[i] = 0;
+            }
+            MPI_Send(saco[0], c, MPI_INT, i + 1, KILL, MPI_COMM_WORLD);
+        }
+        
+        t_final = MPI_Wtime();
+        printf("[Mestre] : Tempo decorrido %.4f\n", t_final - t_inicial);
+    }
+    else
+    {
+        // ESCRAVO
+        while (1)
+        {
+            MPI_Recv(message, c, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
-    // MPI_Finalize();
+            if (status.MPI_TAG == TRABALHO)
+            {
+                printf("[%d] : Recebendo tarefa\n", my_rank);
+                qsort(message, c, sizeof(int), cmpfunc); // quicksort
+                MPI_Send(message, c, MPI_INT, 0, TRABALHO, MPI_COMM_WORLD);
+            }
+            else if (status.MPI_TAG == KILL)
+            {
+                printf("[%d] : KILL \n", my_rank);
+                break;
+            }
+        }
+    }
+
+    MPI_Finalize();
     free(saco);
+    free(message);
     return 0;
 }
