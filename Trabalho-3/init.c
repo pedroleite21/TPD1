fe165b6ca6d897970b1195825cccd70e0bf6159d
16104078 <16104078@facin.portoalegre.pucrsnet.br>
2019-05-30 18:53:14 -0300

    Codigo sequencial

--- Trabalho-3/init.c
+++ Trabalho-3/init.c
@@ -1,58 +1,77 @@
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-#define DEBUG 1            // comentar esta linha quando for medir tempo
-#define ARRAY_SIZE 40      // trabalho final com o valores 10.000, 100.000, 1.000.000
+#define DEBUG 1           // comentar esta linha quando for medir tempo
+#define ARRAY_SIZE 100000 // trabalho final com o valores 10.000, 100.000, 1.000.000
 
-int cmpfunc (const void * a, const void * b) {
-   return ( *(int*)a - *(int*)b );
+#define M 1000
+#define N 10000
+
+int cmpfunc(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
 }
 
-
-void bs(int n, int * vetor)
+void bs(int n, int *vetor)
 {
-    int c=0, d, troca, trocou =1;
+    int c = 0, d, troca, trocou = 1;
 
-    while (c < (n-1) & trocou )
-        {
+    while (c < (n - 1) & trocou)
+    {
         trocou = 0;
-        for (d = 0 ; d < n - c - 1; d++)
-            if (vetor[d] > vetor[d+1])
-                {
-                troca      = vetor[d];
-                vetor[d]   = vetor[d+1];
-                vetor[d+1] = troca;
+        for (d = 0; d < n - c - 1; d++)
+            if (vetor[d] > vetor[d + 1])
+            {
+                troca = vetor[d];
+                vetor[d] = vetor[d + 1];
+                vetor[d + 1] = troca;
                 trocou = 1;
-                }
+            }
         c++;
-        }
+    }
 }
 
 int main()
 {
     int vetor[ARRAY_SIZE];
-    int i;
+    int i, j;
 
-    for (i=0 ; i<ARRAY_SIZE; i++)              /* init array with worst case for sorting */
-        vetor[i] = ARRAY_SIZE-i;
-    
+    int l = M, c = N;
+    int(*saco)[c] = malloc(l * sizeof *saco);
 
-    #ifdef DEBUG
-    printf("\nVetor: ");
-    for (i=0 ; i<ARRAY_SIZE; i++)              /* print unsorted array */
-        printf("[%03d] ", vetor[i]);
-    #endif
+    clock_t begin = clock();
+    double time_spent;
+    clock_t end;
 
-	qsort(vetor, ARRAY_SIZE, sizeof(int), cmpfunc);  /* quicksort */
+    for (i = 0; i < l; i++)
+        for (j = 0; j < c; j++)
+            saco[i][j] = c - j;
 
-    //bs(ARRAY_SIZE, vetor);                     /* sort array */
+    // #ifdef DEBUG
+    //     printf("\nVetor: ");
+    //     for (i = 0; i < ARRAY_SIZE; i++) /* print unsorted array */
+    //         printf("[%03d] ", vetor[i]);
+    // #endif
 
+    for(i=0;i<l;i++){
+        //qsort(saco[i], c, sizeof(int), cmpfunc);
+		bs(c, saco[i]);
+    }
 
-    #ifdef DEBUG
-    printf("\nVetor: ");
-    for (i=0 ; i<ARRAY_SIZE; i++)                              /* print sorted array */
-        printf("[%03d] ", vetor[i]);
-    #endif
+    // #ifdef DEBUG
+    //     printf("\nVetor: ");
+    //     for (i = 0; i < ARRAY_SIZE; i++) /* print sorted array */
+    //         printf("[%03d] ", vetor[i]);
+    // #endif
+
+    end = clock();
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Tempo de execucao: %f seconds\n", time_spent);
+
+    free(saco);
 
     return 0;
 }
